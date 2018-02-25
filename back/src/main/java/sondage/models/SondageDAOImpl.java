cff558fcf097e421ca51c99c768da84d1fd15745
nfnxmc <mamoudou.n.camara@gmail.com>
2018-02-26 00:14:59 +0100

    SondageDAO implemented

--- back/src/main/java/sondage/models/SondageDAOImpl.java
+++ back/src/main/java/sondage/models/SondageDAOImpl.java
@@ -1,29 +1,61 @@
 package sondage.models;
 
-public class SondageDAOImpl implements SondageDAO {
+import javax.persistence.TypedQuery;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SondageDAOImpl implements SondageDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public int modifier(Sondage sondage) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session session = this.sessionFactory.getCurrentSession();
+		TypedQuery<Sondage> query = (TypedQuery<Sondage>) session.getNamedQuery("modifierSondage");
+		query.setParameter("titre", sondage.getTitre());
+		query.setParameter("question", sondage.getQuestion());
+		query.setParameter("choix", sondage.getChoix());
+		int total = query.getMaxResults();
+		return total;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public int creer(Sondage sondage) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session session = this.sessionFactory.getCurrentSession();
+		TypedQuery<Sondage> query = (TypedQuery<Sondage>) session.getNamedQuery("creerSondage");
+		query.setParameter("titre", sondage.getTitre());
+		query.setParameter("question", sondage.getQuestion());
+		query.setParameter("choix", sondage.getChoix());
+		int total = query.getMaxResults();
+		return total;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public int supprimer(Sondage sondage) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session session = this.sessionFactory.getCurrentSession();
+		TypedQuery<Sondage> query = (TypedQuery<Sondage>) session.getNamedQuery("supprimerSondage");
+		query.setParameter("titre", sondage.getTitre());
+		
+		int total = query.getMaxResults();
+		return total;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Sondage chercher(String titre) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = this.sessionFactory.getCurrentSession();
+		TypedQuery<Sondage> query = (TypedQuery<Sondage>) session.getNamedQuery("supprimerSondage");
+		query.setParameter("titre", titre);
+		
+		Sondage sondage = query.getSingleResult();
+		
+		return sondage;
 	}
 
 }

509455fcb9f5b12f3ac9711a32220c1d34645f35
nfnxmc <mamoudou.n.camara@gmail.com>
2018-02-25 23:34:16 +0100

    SondageDAO not implemented yet!

---
+++ back/src/main/java/sondage/models/SondageDAOImpl.java
@@ -0,0 +1,29 @@
+package sondage.models;
+
+public class SondageDAOImpl implements SondageDAO {
+
+	@Override
+	public int modifier(Sondage sondage) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int creer(Sondage sondage) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int supprimer(Sondage sondage) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Sondage chercher(String titre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

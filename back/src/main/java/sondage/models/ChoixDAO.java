f627cc18ea73ca309a506ff8e43f60c667b379c1
nfnxmc <mamoudou.n.camara@gmail.com>
2018-02-27 11:19:03 +0100

    Initial Commit

--- back/src/main/java/sondage/models/ChoixDAO.java
+++
@@ -1,9 +0,0 @@
-package sondage.models;
-
-public interface ChoixDAO {
-
-	public int modifier(Choix choix);
-	public int creer(Choix choix);
-	public int supprimer(Choix choix);
-	public Choix chercher(String titre);
-}
--- back/src/main/java/sondage/models/SondageDAO.java
+++
@@ -1,9 +0,0 @@
-package sondage.models;
-
-public interface SondageDAO {
-
-	public int modifier(Sondage sondage);
-	public int creer(Sondage sondage);
-	public int supprimer(Sondage sondage);
-	public Sondage chercher(String titre);
-}
